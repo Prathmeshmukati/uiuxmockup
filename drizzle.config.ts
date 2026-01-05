92553ae5d66c5e0b602f23195726b8d9201ae255
ai-trippanner <prathmeshmukati15@gmail.com>
2026-01-05 15:08:42 +0530

    database setup with neondb

---
+++ drizzle.config.ts
@@ -0,0 +1,13 @@
+import 'dotenv/config';
+import { defineConfig } from 'drizzle-kit';
+
+export default defineConfig({
+  schema: './config/schema.tsx',
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+});
+
+
+
